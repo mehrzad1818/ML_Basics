291c379eaf121a255c5246bb5be048a84862b2e8
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-09-21 23:48:57 +0330

    Create Preprocessing.py

---
+++ Preprocessing.py
@@ -0,0 +1,10 @@
+""" Data Preprocessing"""
+
+# Importing our required libraries
+
+import numpy as np  # Contains mathematical tools
+import matplotlib.pyplot as plt  # Contains plotting tools
+import pandas as pd  # Contains tools for importing and managing datasets
+
+
+data_set = pd.read_csv("Data.csv")
